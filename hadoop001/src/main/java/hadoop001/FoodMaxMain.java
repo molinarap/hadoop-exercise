58e85869442fd2283bd95add34469d8358d82cff
Paolo Molinara <pmolinara@gmail.com>
2016-04-29 02:08:28 +0200

    cambiato il nome del package

---
+++ hadoop001/src/main/java/hadoop001/FoodMaxMain.java
@@ -0,0 +1,31 @@
+package hadoop001;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class FoodMaxMain {
+
+    public static void main(String[] args) throws Exception {
+    	
+    	Job job = new Job(new Configuration(), "FoodMaxMain");
+
+		job.setJarByClass(FoodMaxMain.class);
+		
+		job.setMapperClass(FoodMaxMapper.class);
+		job.setCombinerClass(FoodMaxCombiner.class);
+		job.setReducerClass(FoodMaxReducer.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		job.waitForCompletion(true);
+    }
+}
